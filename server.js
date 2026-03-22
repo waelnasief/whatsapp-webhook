ba1219518ff39c2518a09663b1ae391abfc4750f
waelnasief <waelnasief2000@yahoo.com>
2026-03-22 18:17:38 +0300

    Fix port for Render

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ app.post("/webhook", (req, res) => {
   return res.sendStatus(200);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
