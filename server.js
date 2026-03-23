cf175f290fdc17813b7fdb911326fc076552c0e0
waelnasief <waelnasief2000@yahoo.com>
2026-03-24 01:53:42 +0300

    add auto reply

--- server.js
+++ server.js
@@ -22,10 +22,45 @@ app.get("/webhook", (req, res) => {
   return res.status(403).send("Forbidden");
 });
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", async (req, res) => {
   console.log("Incoming webhook:");
   console.log(JSON.stringify(req.body, null, 2));
-  return res.sendStatus(200);
+
+  try {
+    const entry = req.body.entry?.[0];
+    const changes = entry?.changes?.[0];
+    const message = changes?.value?.messages?.[0];
+
+    if (message) {
+      const from = message.from;
+
+      console.log("Message from:", from);
+
+      // 👇 reply
+      const response = await fetch(
+         `https://graph.facebook.com/v18.0/1069337116257766/messages`,
+        {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer EAALkNdaYHbMBRJVdU4y3o8KQR6EfuZC1wBCHlvFZAiyme4PP3wZA7b0m7VR7y1W0ipiULfn0BEr8f9YSbscvcsG4Y7eeI3LKefM0iuALoi7VE8VFMtv8dyMZBlhroyutAXsqlO3Ob69Yy7TIGuACB1AyaXDBEAjMyHowN2JExEZANl6irJ1EWkOgCx244QL6qhQ3x2IW8ZAOzBDMStfapOOsuraKE7LRZCbzZA1s3A5PtAG0oHkePCNgwcBeVr30AmpVPaAGNMZBt5x681oSnztp2CyHh`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            messaging_product: "whatsapp",
+            to: from,
+            text: { body: "Hello 👋 Thanks for contacting us!" },
+          }),
+        }
+      );
+
+      const data = await response.json();
+      console.log("Reply sent:", data);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.sendStatus(200);
 });
 
 const PORT = process.env.PORT || 3000;
